da49be02aa950d43df50cf0772eea55177dc9c46
Mick Thompson <dthompson@gmail.com>
2012-03-20 11:00:10 -0700

    remove 10 thing limit, and load once the map is ready

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -5,14 +5,14 @@ class Thing < ActiveRecord::Base
   belongs_to :user
   has_many :reminders
 
-  def self.find_closest(lat, lng, limit=10)
+  def self.find_closest(lat, lng, limit=200)
     query = <<-SQL
       SELECT *, (3959 * ACOS(COS(RADIANS(?)) * COS(RADIANS(lat)) * COS(radians(lng) - RADIANS(?)) + SIN(RADIANS(?)) * SIN(RADIANS(lat)))) AS distance
       FROM things
       ORDER BY distance
       LIMIT ?
       SQL
-    find_by_sql([query, lat.to_f, lng.to_f, lat.to_f, limit.to_i])
+    find_by_sql([query, lat.to_f, lng.to_f, lat.to_f, 200])
   end
 
   def reverse_geocode
