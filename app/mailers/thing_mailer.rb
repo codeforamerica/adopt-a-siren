0925f2fe13c74e0a22d2b5c895531e5bacbc7e88
Kyle Shimabukuro <kshimabukuro@honolulu.gov>
2012-04-24 10:03:00 -1000

    - Added escapes for public's and City's

--- app/mailers/thing_mailer.rb
+++ app/mailers/thing_mailer.rb
@@ -29,11 +29,11 @@ During an actual emergency these broadcasts will be heard at frequent intervals 
 
 In addition, residents in areas surrounding Campbell Industrial Park, Honokai Hale, Makakilo, Kapolei Regional Park, Kapolei Golf Course, and the Coast Guard Station at Kalaeloa may also hear a \"whooping\" tone following the Siren Test. This \"whooping\" tone is a test of the Hazardous Materials (HAZMAT) Incident outdoor siren warning group that will be activated in the event of a HAZMAT incident.
 
-Siren Malfunction: If the siren in your community does not sound or does not operate properly please call the Department of Emergency Management at 723-8960 to report it. You can also email the department at dem@honolulu.gov. With more than 170 outdoor warning sirens on Oahu we appreciate the public’s assistance in identifying problem units.
+Siren Malfunction: If the siren in your community does not sound or does not operate properly please call the Department of Emergency Management at 723-8960 to report it. You can also email the department at dem@honolulu.gov. With more than 170 outdoor warning sirens on Oahu we appreciate the public\'s assistance in identifying problem units.
 
 Siren Damage or Vandalism: You can help us to safeguard our Outdoor Siren Warning System. Please report any acts of vandalism, damages, or missing sirens or components to the Department of Emergency Management at 723-8960. You can also email the department at dem@honolulu.gov and include any images you may have of the siren in question. Any suspicious activity should be reported immediately to the Honolulu Police Department by calling 911.
 
-On-line Siren Reporting: Residents now have the option of reporting malfunctioning or vandalized sirens on-line. Visit the City’s Siren Trouble Report page at http://www3.honolulu.gov/DEMSiren/ to file your report as well as upload pictures.
+On-line Siren Reporting: Residents now have the option of reporting malfunctioning or vandalized sirens on-line. Visit the City\'s Siren Trouble Report page at http://www3.honolulu.gov/DEMSiren/ to file your report as well as upload pictures.
 
 Mahalo!")
   end
