69cd841d358f6df48011fc48ae9b963ee122e719
Kyle Shimabukuro <kshimabukuro@honolulu.gov>
2012-04-24 08:55:49 -1000

    - Modified application.rb file

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,14 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+=begin
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
+=end
 
 module AdoptAThing
   class Application < Rails::Application
@@ -50,11 +52,13 @@ module AdoptAThing
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+=begin
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+=end
   end
 end
