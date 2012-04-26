d5ac1126d14ed1257fa33504bdc2a8854b578262
Kyle Shimabukuro <kshimabukuro@honolulu.gov>
2012-04-26 10:57:47 -1000

    - Cleaned up thing_mailer.rb and scheduler.rake

--- app/mailers/thing_mailer.rb
+++ app/mailers/thing_mailer.rb
@@ -14,8 +14,8 @@ class ThingMailer < ActionMailer::Base
   end
 =end
 
-  def send_reminder
-    if ['2012-04-24'].include? Date.current.to_s
+  def send_generic
+    if ['2012-04-27', '2012-05-01', '2012-06-01', '2012-07-02', '2012-08-01', '2012-09-04', '2012-10-01', '2012-11-01', '2012-12-03'].include? Date.current.to_s
     things = Thing.where("user_id IS NOT NULL")
     emails = (things.all.collect {|t| t.user.email}).join(", ")
     mail(:bcc => emails, :subject => "Please remember to listen for your adopted siren today at 11:45AM", :body => "Aloha Kakahiaka!
@@ -40,8 +40,8 @@ Mahalo!")
     end
   end
 
-  def send_individual
-    if ['2012-04-25'].include? Date.current.to_s
+  def send_personalized
+    if ['2012-04-27', '2012-05-01', '2012-06-01', '2012-07-02', '2012-08-01', '2012-09-04', '2012-10-01', '2012-11-01', '2012-12-03'].include? Date.current.to_s
       things = Thing.where("user_id IS NOT NULL")
       things.each do |t|
       emails = t.user.email
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,12 +1,9 @@
-desc "This task is called by the Heroku scheduler add-on to send out monthly reminders"
-task :send_monthly_reminder => :environment do
-
-  ThingMailer.send_reminder.deliver
-
+desc "This task is called by the Heroku scheduler add-on to send out generic monthly reminders"
+task :send_monthly_generic => :environment do
+  ThingMailer.send_generic.deliver
 end
 
-task :send_monthly_individual => :environment do
-
-  ThingMailer.send_individual
-
+desc "This task is called by the Heroku scheduler add-on to send out personalized monthly reminders"
+task :send_monthly_personalized => :environment do
+  ThingMailer.send_personalized
 end
