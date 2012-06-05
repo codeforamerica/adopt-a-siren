6106f7a71ee608cfa432996880ac4777838e625a
Mick Thompson <dthompson@gmail.com>
2012-06-05 09:52:16 -1000

    add env to set off forced email, bypassind date check

--- app/mailers/thing_mailer.rb
+++ app/mailers/thing_mailer.rb
@@ -28,21 +28,20 @@ Mahalo!")
   end
 
   def send_personalized
-#    if ['2012-04-27', '2012-05-01', '2012-06-01', '2012-07-02', '2012-08-01', '2012-09-04', '2012-10-01', '2012-11-01', '2012-12-03'].include? Date.current.to_s
-    things = Thing.joins(:users)
+    if ENV.include? 'FORCE_EMAIL' or ['2012-04-27', '2012-05-01', '2012-06-01', '2012-07-02', '2012-08-01', '2012-09-04', '2012-10-01', '2012-11-01', '2012-12-03'].include? Date.current.to_s
+      things = Thing.joins(:users)
       
-    things.each do |t|
-      t.users.each do |u|
-        emails = u.email
-        print emails
-        @thing_name = t.name
-        mail(:to => emails, :subject => "Please remember to listen for your adopted siren today at 11:45AM").deliver
-      end
-
+      things.each do |t|
+        t.users.each do |u|
+          emails = u.email
+          print emails
+          @thing_name = t.name
+          mail(:to => emails, :subject => "Please remember to listen for your adopted siren today at 11:45AM").deliver
+        end
+      end      
+    else
+      print "skipping"
     end
-
-    #end
-
+    
   end
-
 end
