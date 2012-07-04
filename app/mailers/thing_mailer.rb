56d30226518f01b1bc8ab4fc75748d645188a326
Mick Thompson <dthompson@gmail.com>
2012-07-04 10:16:29 -0600

    change from email address

--- app/mailers/thing_mailer.rb
+++ app/mailers/thing_mailer.rb
@@ -1,5 +1,5 @@
 class ThingMailer < ActionMailer::Base
-  default :from => "adoptasiren@honolulu.gov"
+  default :from => "dem@honolulu.gov"
 
   def send_personalized
     if ENV.include? 'FORCE_EMAIL' or ['2012-04-27', '2012-05-01', '2012-06-01', '2012-07-02', '2012-08-01', '2012-09-04', '2012-10-01', '2012-11-01', '2012-12-03'].include? Date.current.to_s
